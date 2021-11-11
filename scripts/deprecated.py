6beadd5e1a723d536017b8499b1250d27d582d4e
Corentin Delorme <codelorme@gmail.com>
2021-11-11 21:39:05 +0100

    Add script deprecated.py

---
+++ scripts/deprecated.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from os import chdir
+from os.path import dirname, realpath
+
+chdir(dirname(realpath(__file__)))
+chdir('..')
+
+with open('addevent.txt', 'r') as fd:
+
+	events = []
+	for event in fd.read().split('\n'):
+		if not event:
+			continue
+		events.append(event)
+
+with open('triggerevent.txt', 'r') as fd:
+
+	triggered_event = []
+	for event in fd.read().split('\n'):
+		if not event:
+			continue
+
+		triggered_event.append(event.split(';')[1])
+
+with open('deprecated.txt', 'w') as fd:
+	for event in events:
+		if event not in triggered_event:
+			fd.write(event + '\n')
