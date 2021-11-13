0b304f35c61130069c3b14e6cbd2e7bec96156bb
Corentin Delorme <codelorme@gmail.com>
2021-11-13 16:08:38 +0100

    Rename script addevent to addeventjava + Rename addevent.txt to addeventjava.txt

--- scripts/parsedescriptions.py
+++ scripts/parsedescriptions.py
@@ -11,7 +11,7 @@ chdir('..')
 with open('savedevents.json', 'r') as fd:
 	savedevents = json.loads(fd.read())
 
-with open('addevent.txt') as fd:
+with open('addeventjava.txt') as fd:
 	events = [ x for x in fd.read().split('\n') if x.strip() ]
 
 db = {}
