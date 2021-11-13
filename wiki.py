f2e8f03071d6fb9ff5ad51cb0309a25606fe2c72
Corentin Delorme <codelorme@gmail.com>
2021-11-13 02:06:57 +0100

    Update wiki.py: Big refactoring

--- wiki.py
+++ wiki.py
@@ -7,6 +7,9 @@ import requests
 from os import walk
 from os.path import basename, join, sep
 from urllib.parse import urljoin
+from retry import retry
+
+tries = 5
 
 class Wiki:
 
@@ -17,12 +20,16 @@ class Wiki:
 
 		target = config.get('target', {})
 
-		self.api_path = target.get('wiki_path', '/w/api.php')
-		self.api_url  = target['wiki_url'].strip('/')
-		self.logged   = False
-		self.password = target['password']
-		self.username = target['username']
-		self.session  = requests.Session()
+		self.api_path    = target.get('wiki_path', '/w/api.php')
+		self.api_url     = target['wiki_url'].strip('/')
+		self.logged      = False
+		self.javadoc_url = config['java']['javadoc_url']
+		self.obsolete    = self.get_obsolete_events()
+		self.oracle_url  = config['java']['oracle_url']
+		self.packages    = self.get_packages()
+		self.password    = target['password']
+		self.username    = target['username']
+		self.session     = requests.Session()
 
 		if not self.api_url.endswith(self.api_path):
 			self.api_url = urljoin(self.api_url, self.api_path)
@@ -32,6 +39,32 @@ class Wiki:
 		errorinfo = jsondata['error']['info']
 		raise Exception(errorcode + ': ' + errorinfo)
 
+	@retry(tries=tries)
+	def get_csrf_token(self):
+
+		params = {
+			'action': 'query',
+			'meta':   'tokens',
+			'format': 'json',
+		}
+
+		jsondata = self.session.get(self.api_url, params=params).json()
+		return jsondata['query']['tokens']['csrftoken']
+
+	def get_javadoc_url(self, type):
+
+
+		pkg = self.packages.get(type)
+		if type == '???' or not pkg:
+			if type != '???':
+				print('MISSING TYPE: %s %s' % (type, pkg))
+			return ''
+
+		pkg_url = pkg.replace('.', '/') + '.html'
+		javadoc_url = pkg.startswith('zomb') and self.javadoc_url or self.oracle_url
+		return urljoin(javadoc_url, pkg_url)
+
+	@retry(tries=tries)
 	def get_login_token(self):
 
 		params = {
@@ -43,6 +76,14 @@ class Wiki:
 
 		jsondata = self.session.get(self.api_url, params=params).json()
 		return jsondata['query']['tokens']['logintoken']
+
+	def get_obsolete_events(self):
+		with open('deprecated.txt', 'r') as fd:
+			return fd.read().split('\n')
+
+	def get_packages(self):
+		with open('class2pkg.json', 'r') as fd:
+			return json.loads(fd.read())
 
 	def login(self):
 
@@ -57,60 +98,97 @@ class Wiki:
 		self.session.post(self.api_url, data=data)
 		self.logged = True
 
-	def get_csrf_token(self):
+	def format_event_description(self, jsonevent):
 
-		params = {
-			'action': 'query',
-			'meta':   'tokens',
-			'format': 'json',
-		}
+		desc = jsonevent.get('description', 'TODO')
+		if 'parameters' not in jsonevent:
+			return desc
 
-		jsondata = self.session.get(self.api_url, params=params).json()
-		return jsondata['query']['tokens']['csrftoken']
+		for param in jsonevent['parameters']:
+			name = param['name']
+			type = param['type']
+			desc = desc.replace('[[' + type + ']]', name).replace(type, name)
 
+		additional_info = jsonevent.get('additional_info', '')
+		return '\n'.join([desc, additional_info])
 
-	def format_event_page(self, title, jsondata):
+	def format_event_example(self, jsonevent):
 
-		name = basename(title)
+		event = jsonevent['name']
+		func = event[0].lower() + event[1:]
+
+		output  = ''
+		output += 'function ' + func + '('
+
+		if 'parameters' in jsonevent:
+			params = []
+			for param in jsonevent['parameters']:
+				params.append(param['name'])
+			output += ', '.join(params)
+
+		output += ')\n'
+		output += '\t-- Your code here\n'
+		output += 'end\n\n'
+		output += 'Events.' + event + '.Add(' + func + ')'
+
+		return '<syntaxhighlight lang="lua">' + output + '</syntaxhighlight>'
+
+	def format_event_page(self, jsonevent):
+
 		output  = '{{Function\n'
-		output += '|name = %s\n' % name
-		output += '|description = %s\n' % jsondata['description']
-		output += '|parameters = '
-
-		if 'parameters' not in jsondata:
-			output += 'No parameter.\n'
-		else:
-			output += '\n'
-			for param in jsondata['parameters']:
-				output += '* %s\n' % param
-
-		output += '|example = %s\n' % jsondata.get('example', 'TODO')
-		output += '|additional_info = %s\n' % jsondata.get('additional_info', 'No additional information.')
-		output += '|see_also = '
-		if 'see_also' not in jsondata:
-			output += 'No reference.\n'
-		else:
-			output += '\n'
-			for see_also in jsondata['see_also']:
-				output += '* %s\n' % see_also
-
+		output += '|name = %s\n'             % jsonevent['name']
+		output += '|description = %s\n'      % self.format_event_description(jsonevent)
+		output += '|parameters =\n%s\n'      % self.format_event_parameters(jsonevent)
+		output += '|example =\n%s\n'         % self.format_event_example(jsonevent)
+		output += '|see_also =\n%s\n'        % self.format_event_see_also(jsonevent)
 		output += '}}\n\n'
-		output += '[[Category:Lua Event]]\n'
+		output += '[[Category:Lua Events]]\n'
 
 		return output
 
-	def edit_page(self, title, filepath):
+	def format_event_parameters(self, jsonevent):
+
+		if 'parameters' not in jsonevent:
+			return 'No parameter.\n'
+
+		result = []
+		jsonparams = jsonevent['parameters']
+		for jsonparam in jsonparams:
+			desc = jsonparam.get('description', '')
+			name = jsonparam['name']
+			types = []
+			for type in jsonparam['type'].split(','):
+				desc = desc.replace('[[' + type + ']]', name)
+				desc = desc.replace(type, name.replace('_', ' '))
+				javadoc_url = self.get_javadoc_url(type)
+				line = javadoc_url and '[%s %s]' % (javadoc_url, type) or '[[Modding:Lua Events/%s|%s]]' % (type, type)
+				types.append(line)
+			type = ' / '.join(types)
+			result.append('* %s %s' % (type, desc))
+
+		return '\n'.join(result)
+
+	def format_event_see_also(self, jsonevent):
+
+		if 'see_also' not in jsonevent:
+			return 'No related event.\n'
+
+		result = []
+		jsondata = jsonevent['see_also']
+		for see_also in jsondata:
+			result.append('* [[Modding:Lua Events/%s|%s]]' % (see_also, see_also))
+
+		return '\n'.join(result)
+
+	@retry(tries=tries)
+	def edit_event_page(self, title, jsonevent):
 
 		print(title)
 
 		if not self.logged:
 			self.login()
 
-		with open(filepath, 'r') as fd:
-			text = fd.read()
-
-		if filepath.endswith('.json'):
-			text = self.format_event_page(title, json.loads(text))
+		text = self.format_event_page(jsonevent)
 
 		data = {
 			'action': 'edit',
@@ -126,6 +204,7 @@ class Wiki:
 
 		time.sleep(self.default_sleep)
 
+	@retry(tries=tries)
 	def delete_page(self, title):
 
 		print(title)
@@ -150,16 +229,10 @@ class Wiki:
 
 		time.sleep(self.default_sleep)
 
-	def update_wiki(self, root='.'):
+	def update_events(self):
 
-		for root, subdirs, files in walk(root):
+		with open('events.json', 'r') as fd:
+			jsondata = json.loads(fd.read())
 
-			if root == '.':
-				continue
-
-			root = root[2:]
-
-			for filename in files:
-				title = sep.join([root, filename.replace('.json', '').replace('.txt', '')]).replace(sep, ':', 1)
-				filepath = join(root, filename)
-				self.edit_page(title, filepath)
+		for title, jsonevent in jsondata.items():
+			self.edit_event_page(title, jsonevent)
