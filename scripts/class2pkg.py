515a9a1824d3207d8eb17dd6b970dabe98ce61e7
Corentin Delorme <codelorme@gmail.com>
2021-11-15 05:08:07 +0100

    Update scripts class2pkg.py and deprecated.py

--- scripts/class2pkg.py
+++ scripts/class2pkg.py
@@ -2,7 +2,7 @@
 
 import json
 
-from os import chdir, getcwd, walk
+from os import chdir, getcwd, makedirs, walk
 from os.path import basename, dirname, join, realpath, sep
 
 chdir(dirname(realpath(__file__)))
@@ -12,42 +12,51 @@ wiki_root = getcwd()
 with open('config.json', 'r') as fd:
 	jsondata = json.loads(fd.read())
 
-ignored_packages = jsondata['java']['ignored_packages']
+ignored_packages = jsondata['ignored_packages']
 
-chdir(jsondata['java']['root'])
+for version in jsondata['versions']:
 
-db = {
-	# Java types
-	'ArrayList':  'java.util',
-	'Boolean':    'java.lang',
-	'ByteBuffer': 'java.nio',
-	'Double':     'java.lang',
-	'Float':      'java.lang',
-	'Integer':    'java.lang',
-	'Long':       'java.lang',
-	'Short':      'java.lang',
-	'String':     'java.lang',
-}
 
-for root, subdirs, files in walk('.'):
-	root = root[2:].replace(sep, '.')
-	pkg = root.split('.')[0]
-	if pkg in ignored_packages:
-		continue
+	javaroot = '%s/%s/java/' % (jsondata['source'], version)
+	chdir(javaroot)
 
-	for f in files:
-		if not f.endswith('.java'):
+	db = {
+		# Java types
+		'ArrayList':  'java.util',
+		'Boolean':    'java.lang',
+		'ByteBuffer': 'java.nio',
+		'Double':     'java.lang',
+		'Float':      'java.lang',
+		'Integer':    'java.lang',
+		'Long':       'java.lang',
+		'Short':      'java.lang',
+		'String':     'java.lang',
+	}
+
+	for root, subdirs, files in walk('.'):
+		root = root[2:].replace(sep, '.')
+		pkg = root.split('.')[0]
+		if pkg in ignored_packages:
 			continue
-		f = f.replace('.java', '')
-		db[f] = root
 
-# Zomboid inner classes
-db['PerkFactory.Perk']           = 'zombie.characters.skills'
-db['PerkFactory.Perks']          = 'zombie.characters.skills'
-db['WeatherPeriod.WeatherStage'] = 'zombie.iso.weather'
+		for f in files:
+			if not f.endswith('.java'):
+				continue
+			f = f.replace('.java', '')
+			db[f] = root
 
-chdir(wiki_root)
+	print(version)
 
-output = join('data', 'json', 'class2pkg.json')
-with open(output, 'w') as fd:
-	fd.write(json.dumps(db, sort_keys=True))
+	# Zomboid inner classes
+	db['PerkFactory.Perk']           = 'zombie.characters.skills'
+	db['PerkFactory.Perks']          = 'zombie.characters.skills'
+	db['WeatherPeriod.WeatherStage'] = 'zombie.iso.weather'
+
+	chdir(wiki_root)
+
+	outdir = join('data', 'json', version)
+	makedirs(outdir, exist_ok=True)
+
+	output = join(outdir, 'class2pkg.json')
+	with open(output, 'w') as fd:
+		fd.write(json.dumps(db, sort_keys=True))
