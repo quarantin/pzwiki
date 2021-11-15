7bdbd6e26c00eee78ff76f9f2648d5da06323668
Corentin Delorme <codelorme@gmail.com>
2021-11-15 05:08:07 +0100

    Update scripts class2pkg.py and deprecated.py

--- scripts/deprecated.py
+++ scripts/deprecated.py
@@ -1,32 +1,42 @@
 #!/usr/bin/env python
 
-from os import chdir
+import json
+
+from os import chdir, makedirs
 from os.path import dirname, join, realpath
 
 chdir(dirname(realpath(__file__)))
 chdir('..')
 
-filename = join('data', 'txt', 'addeventjava.txt')
-with open(filename, 'r') as fd:
+with open('config.json', 'r') as fd:
+	jsondata = json.loads(fd.read())
 
-	events = []
-	for event in fd.read().split('\n'):
-		if not event:
-			continue
-		events.append(event)
+for version in jsondata['versions']:
 
-filename = join('data', 'txt', 'triggerevent.txt')
-with open(filename, 'r') as fd:
+	outdir = join('data', 'txt', version)
+	makedirs(outdir, exist_ok=True)
 
-	triggered_event = []
-	for event in fd.read().split('\n'):
-		if not event:
-			continue
+	filename = join(outdir, 'addeventjava.txt')
+	with open(filename, 'r') as fd:
 
-		triggered_event.append(event.split(';')[1])
+		events = []
+		for event in fd.read().split('\n'):
+			if not event:
+				continue
+			events.append(event)
 
-filename = join('data', 'txt', 'deprecated.txt')
-with open(filename, 'w') as fd:
-	for event in events:
-		if event not in triggered_event:
-			fd.write(event + '\n')
+	filename = join(outdir, 'triggerevent.txt')
+	with open(filename, 'r') as fd:
+
+		triggered_event = []
+		for event in fd.read().split('\n'):
+			if not event:
+				continue
+
+			triggered_event.append(event.split(';')[1])
+
+	filename = join(outdir, 'deprecated.txt')
+	with open(filename, 'w') as fd:
+		for event in events:
+			if event not in triggered_event:
+				fd.write(event + '\n')
