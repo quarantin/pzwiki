d8d16bceb5ffdd5f2862bc49e1da90bd1c0b8ae3
Corentin Delorme <codelorme@gmail.com>
2021-12-22 06:44:42 +0100

    Update script luamethods.py: Check all global functions, not just the one in LuaManager.java

--- scripts/luamethods.py
+++ scripts/luamethods.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 
 import json
+import collections
 
-from os import chdir
+from os import chdir, walk
 from os.path import dirname, join, realpath
 
 chdir(dirname(realpath(__file__)))
@@ -12,41 +13,69 @@ with open('config.json') as fd:
 	config = json.loads(fd.read())
 
 version = config['versions'][0]
-source = join(config['source'], version)
+sources = join(config['source'], version, 'sources')
 
 db = {}
-lua_manager = join(source, 'zombie/Lua/LuaManager.java')
-with open(lua_manager) as fd:
 
-	for line in fd:
-		if '@LuaMethod' not in line:
+for root, dirs, files in walk(sources):
+
+	for filename in files:
+
+		if not filename.endswith('.java'):
 			continue
 
-		method_name = line.split('"')[1]
-		lua_method = next(fd).replace('public', '').replace('static', '').strip()
+		source_file = join(sources, root, filename)
+		with open(source_file) as fd:
 
-		return_type, remaining = lua_method.split(' ', 1)
-		params = remaining.split('(', 1)[1]
-		params = [ x.strip() for x in params.replace(')', '').replace('{', '').strip().split(',') ]
-		parameters = []	
-		for param in params:
+			for line in fd:
 
-			if param:
+				if '@LuaMethod' not in line:
+					continue
 
-				param_type, param_name = param.split(' ', 1)
 
-				parameters.append({
-					'name': param_name,
-					'type': param_type,
+				declaration = next(fd).strip() #.replace('public', '').replace('static', '').strip()
+				while declaration:
+					if not declaration.startswith('@'):
+						break
+					declaration = next(fd).strip()
+
+				is_public = 'public ' in declaration
+				is_static = 'static ' in declaration
+
+				modifiers = []
+				if is_public:
+					modifiers.append('public')
+					declaration = declaration.replace('public ', '')
+				if is_static:
+					modifiers.append('static')
+					declaration = declaration.replace('static ', '')
+
+				return_type, remaining = declaration.strip().split(' ', 1)
+				method_name = remaining.split('(', 1)[0]
+				params = remaining.split('(', 1)[1]
+				params = [ x.strip() for x in params.replace(')', '').replace('{', '').strip().split(',') ]
+				parameters = []	
+				for param in params:
+
+					if param:
+
+						param_type, param_name = param.split(' ', 1)
+
+						parameters.append({
+							'name': param_name,
+							'type': param_type,
+							'description': 'TODO',
+						})
+
+				db[method_name] = {
 					'description': 'TODO',
-				})
+					'modifiers': modifiers,
+					'parameters': parameters,
+					'return': return_type,
+				}
 
-		db[method_name] = {
-			'description': 'TODO',
-			'parameters': parameters,
-			'return': return_type,
-		}
+sorted_db = collections.OrderedDict(sorted(db.items(), key=lambda x: x[0].lower()))
 
 output = join('data', 'json', 'luamethods.json')
 with open(output, 'w') as fd:
-	fd.write(json.dumps(db, indent='\t', sort_keys=True))
+	fd.write(json.dumps(sorted_db, indent='\t'))
